249dba2df65c41c0b06e564d12f22520af6a4bf0
Ye Li <ye.li@nxp.com>
2018-05-15 02:03:19 -0700

    MLK-18316 imx8: Fix USBOTG disconnect when XRDC blocking is enabled
    We clean USBOTG register USBCMD if it is used in serial download mode.
    When XRDC blocking is enabled, we can't write this register directly,
    must enable the OTG power, otherwise the kernel will get SError
    exception in mfgtool.
    
    Signed-off-by: Ye Li <ye.li@nxp.com>
    (cherry picked from commit cf2143dc97b2a8f21b828c7386c59ee965d981f2)
    
    Conflicts:
    	arch/arm/mach-imx/imx8/cpu.c

--- arch/arm/mach-imx/imx8/cpu.c
+++ arch/arm/mach-imx/imx8/cpu.c
@@ -1387,3 +1387,27 @@ int get_mmc_env_devno(void)
 }
 #endif
 
+void disconnect_from_pc(void)
+{
+	int ret;
+	struct power_domain pd;
+
+	if (!power_domain_lookup_name("conn_usb0", &pd)) {
+		ret = power_domain_on(&pd);
+		if (ret) {
+			printf("conn_usb0 Power up failed! (error = %d)\n", ret);
+			return;
+		}
+
+		writel(0x0, USB_BASE_ADDR + 0x140);
+
+		ret = power_domain_off(&pd);
+		if (ret) {
+			printf("conn_usb0 Power off failed! (error = %d)\n", ret);
+			return;
+		}
+	} else {
+		printf("conn_usb0 finding failed!\n");
+		return;
+	}
+}
