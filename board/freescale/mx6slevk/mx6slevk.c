nxp-imx/uboot-imx

MLK-17082-02 imx: add optee support for imx6sl
  Add defconfig for tee support;
  Enable the TZASC support;
  Add env config for tee support.

Signed-off-by: Bai Ping <ping.bai@nxp.com>
Reviewed-by: Peng Fan <peng.fan@nxp.com>

board/freescale/mx6slevk/mx6slevk.c
@@ -890,6 +890,11 @@ void i2c_force_reset_slave(void)
 
 int board_late_init(void)
 {
+	setenv("tee", "no");
+#ifdef CONFIG_IMX_OPTEE
+	setenv("tee", "yes");
+#endif
+
 #ifdef CONFIG_ENV_IS_IN_MMC
 	board_late_mmc_env_init();
 #endif

lycheepi/uboot

(cherry picked from commit 3535868b89df84d3e6554114f7867f9ef908ff01)

board/freescale/mx6slevk/mx6slevk.c
@@ -763,6 +763,11 @@ void i2c_force_reset_slave(void)
 
 int board_late_init(void)
 {
+	env_set("tee", "no");
+#ifdef CONFIG_IMX_OPTEE
+	env_set("tee", "yes");
+#endif
+
 #ifdef CONFIG_ENV_IS_IN_MMC
 	board_late_mmc_env_init();
 #endif