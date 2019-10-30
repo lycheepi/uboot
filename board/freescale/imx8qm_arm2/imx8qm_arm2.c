alejandrolozano2/GSOM

MLK-19064 imx8qm: Add secure boot environment
Add the secure boot relevant environment variables to ARM2 and MEK.
When CONFIG_AHAB_BOOT is enabled, we will switch to boot flow:
1. Load the signed OS container to address 0x88000000
2. Using auth_cntr to authenticate the OS container. It will load the
   kernel and FDT to destination addresses.
3. Using booti to boot kernel.

Signed-off-by: Ye Li <ye.li@nxp.com>

board/freescale/imx8qm_arm2/imx8qm_arm2.c
@@ -658,6 +658,11 @@ int board_late_init(void)
 	setenv("board_rev", "iMX8QM");
 #endif
 
+	setenv("sec_boot", "no");
+#ifdef CONFIG_AHAB_BOOT
+	setenv("sec_boot", "yes");
+#endif
+
 #ifdef CONFIG_ENV_IS_IN_MMC
 	board_late_mmc_env_init();
 #endif

board/freescale/imx8qm_mek/imx8qm_mek.c
@@ -494,6 +494,11 @@ int board_late_init(void)
 	setenv("board_rev", "iMX8QM");
 #endif
 
+	setenv("sec_boot", "no");
+#ifdef CONFIG_AHAB_BOOT
+	setenv("sec_boot", "yes");
+#endif
+
 #ifdef CONFIG_ENV_IS_IN_MMC
 	board_late_mmc_env_init();
 #endif

lycheepi/uboot

(cherry picked from commit eb47b72c1c89deddeaa3b3618d8c28aca05cd4ad)

board/freescale/imx8qm_arm2/imx8qm_arm2.c
@@ -640,6 +640,11 @@ int board_late_init(void)
 	env_set("board_rev", "iMX8QM");
 #endif
 
+	env_set("sec_boot", "no");
+#ifdef CONFIG_AHAB_BOOT
+	env_set("sec_boot", "yes");
+#endif
+
 #ifdef CONFIG_ENV_IS_IN_MMC
 	board_late_mmc_env_init();
 #endif

board/freescale/imx8qm_mek/imx8qm_mek.c
@@ -476,6 +476,11 @@ int board_late_init(void)
 	env_set("board_rev", "iMX8QM");
 #endif
 
+	env_set("sec_boot", "no");
+#ifdef CONFIG_AHAB_BOOT
+	env_set("sec_boot", "yes");
+#endif
+
 #ifdef CONFIG_ENV_IS_IN_MMC
 	board_late_mmc_env_init();
 #endif