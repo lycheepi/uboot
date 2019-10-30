nxp-imx/uboot-imx

MLK-18945-9 imx8mm: Update SOC codes to support LCDIF
Enable the video PLL (594Mhz) and clocks in displaymix. Add the LCDIF clock
set interface to change its dot clock rate.
Update registers header file for LCDIF base address.

Signed-off-by: Ye Li <ye.li@nxp.com>

arch/arm/include/asm/arch-imx8m/imx-regs-imx8mm.h
@@ -7,6 +7,8 @@
 #ifndef __ARCH_IMX8M_REGS_H
 #define __ARCH_IMX8M_REGS_H
 
+#include <asm/imx-common/regs-lcdif.h>
+
 /* Based on version 0.2.2 */
 
 #define M4_BOOTROM_BASE_ADDR	0x007E0000
@@ -105,6 +107,8 @@
 #define MXS_BCH_BASE		0x33004000
 
 #define USB_BASE_ADDR		USB1_BASE_ADDR
+
+#define MXS_LCDIF_BASE		LCDIF_BASE_ADDR
 
 #define IOMUXC_GPR0		(IOMUXC_GPR_BASE_ADDR + 0x00)
 #define IOMUXC_GPR1		(IOMUXC_GPR_BASE_ADDR + 0x04)

lycheepi/uboot

(cherry picked from commit 3c27bc4bfa35dbebee2b5797c9137a2257946eca)

arch/arm/include/asm/arch-imx8m/imx-regs-imx8mm.h
@@ -7,6 +7,8 @@
 #ifndef __ARCH_IMX8M_REGS_H
 #define __ARCH_IMX8M_REGS_H
 
+#include <asm/mach-imx/regs-lcdif.h>
+
 /* Based on version 0.2.2 */
 
 #define ROM_VERSION_A0		0x800
@@ -107,6 +109,8 @@
 #define MXS_BCH_BASE		0x33004000
 
 #define USB_BASE_ADDR		USB1_BASE_ADDR
+
+#define MXS_LCDIF_BASE		LCDIF_BASE_ADDR
 
 #define IOMUXC_GPR0		(IOMUXC_GPR_BASE_ADDR + 0x00)
 #define IOMUXC_GPR1		(IOMUXC_GPR_BASE_ADDR + 0x04)