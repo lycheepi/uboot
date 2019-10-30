nxp-imx/uboot-imx

MLK-18406 fastboot support all partition
uuu can write to any position of mmc
sdps: boot -f ../mkimage_imx8dv/imx-mkimage/iMX8QX/flash.bin

FB: ucmd setenv fastboot_dev mmc
FB: ucmd setenv mmcdev ${emmc_dev}
FB: ucmd mmc dev ${emmc_dev}
FB: flash -raw2sparse all xx.sdcard

Signed-off-by: Frank Li <Frank.Li@nxp.com>

arch/arm/cpu/armv8/imx8/cpu.c
@@ -836,7 +836,7 @@ int mmc_get_env_dev(void)
 		break;
 	default:
 		/* If not boot from sd/mmc, use default value */
-		return CONFIG_SYS_MMC_ENV_DEV;
+		return getenv_ulong("mmcdev", 10, CONFIG_SYS_MMC_ENV_DEV);
 	}
 
 	return board_mmc_get_env_dev(devno);

lycheepi/uboot

(cherry picked from commit ca96e0bd1aea1996904b0a71fb1d74c3f5176929)
Conflicts:
	arch/arm/cpu/armv8/imx8/cpu.c
	drivers/usb/gadget/f_fastboot.c
	include/configs/imx8qxp_mek.h

arch/arm/mach-imx/imx8/cpu.c
@@ -827,7 +827,7 @@ int mmc_get_env_dev(void)
 		break;
 	default:
 		/* If not boot from sd/mmc, use default value */
-		return CONFIG_SYS_MMC_ENV_DEV;
+		return get_env_ulong("mmcdev", 10, CONFIG_SYS_MMC_ENV_DEV);
 	}
 
 	return board_mmc_get_env_dev(devno);